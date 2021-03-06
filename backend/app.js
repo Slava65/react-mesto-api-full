a7b62db476a2172e8ac7c001e07fbfc849c5372b
Slava Zaytcev <slava-sakh@yandex.ru>
2021-03-06 20:09:12 +1100

    1

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const { isCelebrateError } = require('celebrate');
-
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const errorHandler = require('./middlewares/errorHandler');
+const auth = require('./middlewares/auth');
+const registerValidator = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { createUser, login } = require('./controllers/users');
+const NotFoundError = require('./errors/notFoundError');
 require('dotenv').config();
 
 const { PORT } = process.env;
 const app = express();
-
-const cors = require('cors');
-const NotFoundError = require('./errors/notFoundError');
-const { createUser, login } = require('./controllers/users');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const registerValidator = require('./middlewares/validation');
-const auth = require('./middlewares/auth');
-const errorHandler = require('./middlewares/errorHandler');
-const cardRouter = require('./routes/cards');
-const userRouter = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
-
 app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
